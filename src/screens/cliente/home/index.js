c92ad10263a80f6db27efe5eb73a49eb796a7b9e
Guilherme Ataides <guilhermedacatia@gmail.com>
2021-05-31 18:00:43 -0300

    Criando menu de componentes footer

--- src/screens/cliente/home/index.js
+++ src/screens/cliente/home/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import {Text,SafeAreaView} from 'react-native';
+import {Text,SafeAreaView, StyleSheet} from 'react-native';
 import AbrirDrawer from './../../../components/abrirDrawer';
+import BotoesFooter from '../../../components/botoesFooter';
 
 export default () => {
     return(
         <SafeAreaView>
             <AbrirDrawer title="HOME" />
+                
+            <BotoesFooter />
         </SafeAreaView>
         
     );
 }
+
