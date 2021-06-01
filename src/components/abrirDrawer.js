2e10f8fc7af28157a8fc7f38de22296db251d919
otnielhenrique <otniel2014@gmail.com>
2021-06-01 03:46:57 -0300

    Home Tela Cliente

--- src/components/abrirDrawer.js
+++ src/components/abrirDrawer.js
@@ -1,29 +1,55 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, Image, TouchableOpacity,Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import {View, Image, TouchableOpacity, Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
-import AbrirDrawer from './../assets/abrirDrawer.png'
+import AbrirDrawer from './../assets/abrirDrawer.png';
 
-export default (props) => {
-    const navigation = useNavigation();
-    return (
-        <>
-        <View style={{ marginLeft: 20, marginTop:20, marginBottom: 10,flexDirection:'row',marginRight:20}}>
-            
-            <TouchableOpacity onPress={() => navigation.openDrawer()}>
-                <Image source={AbrirDrawer} />
-            </TouchableOpacity>
+export default props => {
+  const navigation = useNavigation();
+  return (
+    <>
+      <View
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          marginLeft: 20,
+          marginTop: 20,
+          marginBottom: 10,
+          flexDirection: 'row',
+          marginRight: 20,
+        }}>
+        <TouchableOpacity onPress={() => navigation.openDrawer()}>
+          <Image source={AbrirDrawer} />
+        </TouchableOpacity>
 
-            <View style={{alignSelf:'center', flex:1,justifyContent:'center',alignItems:'center'}} >
-                <Text style={{
-                    color:'#4cf4fa',
-                    fontWeight:'bold',
-                    fontSize:19
-                }}>{props.title}</Text>
-            </View>
-
+        <View
+          // eslint-disable-next-line react-native/no-inline-styles
+          style={{
+            alignSelf: 'center',
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text
+            // eslint-disable-next-line react-native/no-inline-styles
+            style={{
+              color: '#4cf4fa',
+              fontWeight: 'bold',
+              fontSize: 18,
+            }}>
+            {props.title}
+          </Text>
         </View>
-        <Text style={{width:'100%', backgroundColor:'#4cf4fa',height:1, marginBottom:20}}></Text>
-        </>
-    );
-}
+      </View>
+      <Text
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          width: '100%',
+          backgroundColor: '#4cf4fa',
+          height: 1,
+          marginBottom: 20,
+        }}
+      />
+    </>
+  );
+};
